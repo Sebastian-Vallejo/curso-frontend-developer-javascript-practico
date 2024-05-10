28ac7abc1103ed158fa6845b0130824eef554a7e
Sebastian Vallejo <sebastian.vallejo.cifuentes@gmail.com>
2024-05-10 13:46:05 -0500

    Agregue la funcion toggle para ambos menus tanto mobile como desktop con respectivas media queries para que se oculten cuando se salgan del rango

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const navbar_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
+const mobile_menu_icon = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-menu');
 
 navbar_email.addEventListener('click', toggle_desktop_menu);
+mobile_menu_icon.addEventListener('click', toggle_mobile_menu);
 
-function toggle_desktop_menu(){
+function toggle_desktop_menu() {
 
     console.log('se hizo click');
     desktop_menu.classList.toggle('inactive');
 
+}
+
+function toggle_mobile_menu() {
+    console.log('mobile-menu was clicked');
+    mobile_menu.classList.toggle('inactive');
 }
 
